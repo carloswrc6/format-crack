e3025c6962ff677d190505fb7b474969f3bf365d
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2024-09-07 18:59:17 -0500

    render array | object

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,6 +1,14 @@
 const Line = ({ x1, y1, x2, y2 }) => {
   return (
-    <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="gray" strokeWidth="1" markerEnd="url(#arrow)" />
+    <line
+      x1={x1}
+      y1={y1}
+      x2={x2}
+      y2={y2}
+      stroke="gray"
+      strokeWidth="1"
+      markerEnd="url(#arrow)"
+    />
   );
 };
-export default Line
+export default Line;
