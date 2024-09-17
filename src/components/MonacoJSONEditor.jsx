3d951de0cdace0e9ebe24bfcab833c46f854b83d
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2024-09-17 11:28:16 -0500

    ✨ feat: Monaco Editor

---
+++ src/components/MonacoJSONEditor.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useEffect } from "react";
+import * as monaco from "monaco-editor";
+
+const MonacoJSONEditor = () => {
+  const editorRef = useRef(null);
+  const monacoEditorRef = useRef(null); // Para mantener referencia del editor de Monaco
+
+  useEffect(() => {
+    if (editorRef.current) {
+      monacoEditorRef.current = monaco.editor.create(editorRef.current, {
+        value: JSON.stringify(
+          {
+            "1valorPrimitivo": "valorPrimitivo",
+            "2valorPrimitivo": "valorPrimitivo",
+            "3valorPrimitivo": "valorPrimitivo",
+            objeto: {
+              name: "Eternal Flame",
+              age: 1000000,
+              secretIdentity: "Unknown",
+              arrays: [
+                { 1: 111, 12: 111, "1v": 111 },
+                "222",
+                "333",
+                4,
+                [
+                  { 1: 111, 12: 111, 123: 111 },
+                  "222",
+                  "333",
+                  4,
+                  [{ 1: 1, 12: 1 }, "2", 3, 4],
+                ],
+              ],
+            },
+            arrayx: [["1", 111], "222", "333", 4],
+          },
+          null,
+          2
+        ),
+        language: "json",
+        theme: "vs-dark",
+        automaticLayout: true,
+        minimap: { enabled: true },
+        fontSize: 14,
+        lineNumbers: "on",
+        scrollBeyondLastLine: false,
+        roundedSelection: false,
+        readOnly: false,
+        cursorStyle: "line",
+        wordWrap: "on",
+      });
+    }
+
+    return () => {
+      if (monacoEditorRef.current) {
+        monacoEditorRef.current.dispose(); // Liberar recursos correctamente
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <div ref={editorRef} style={{ height: "400px", width: "100%" }} />
+    </div>
+  );
+};
+
+export default MonacoJSONEditor;
