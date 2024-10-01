b9953ac1f475977f443eaf9273f8a8dd34082cb3
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2024-10-01 11:18:28 -0500

    ✨ feat: Reusable components

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,7 @@
+const CustomButton = () => {
+  return (
+    <button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button> // <div className="App">
+  );
+};
+
+export default CustomButton;
---
+++ src/components/Input.jsx
@@ -0,0 +1,32 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+export default function Example() {
+  return (
+    <div>
+      <div className="relative mt-2 rounded-md shadow-sm">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <span className="text-gray-500 sm:text-sm">Q</span>
+        </div>
+        <input
+          id="price"
+          name="price"
+          type="text"
+          placeholder="0.00"
+          className="block w-40 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+  );
+}
